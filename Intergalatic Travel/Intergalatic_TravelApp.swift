a2083f7e62688d42821559e8d83b51a0a004319b
Chase Hashiguchi <chashiguchi@bsd220.org>
2023-10-24 09:31:18 -0500

    Initial Commit

---
+++ Intergalatic Travel/Intergalatic_TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intergalatic_TravelApp.swift
+//  Intergalatic Travel
+//
+//  Created by chase Hashiguchi on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Intergalatic_TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
